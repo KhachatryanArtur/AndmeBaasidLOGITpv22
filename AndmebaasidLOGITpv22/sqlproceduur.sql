4f8091fa0be44eb8f99db3109c831a3344199ad6
KhachatryanArtur <120027657+KhachatryanArtur@users.noreply.github.com>
2024-04-22 13:03:23 +0300

    Update sqlproceduur.sql

--- AndmebaasidLOGITpv22/sqlproceduur.sql
+++ AndmebaasidLOGITpv22/sqlproceduur.sql
@@ -1,1 +1,60 @@
+create database ArturKhachatryan
+use ArturKhachatryan
+create table film(
+filmId int primary key identity (1,1),
+filmNimetus varchar(50),
+kestvus int,
+rezisoor varchar(50),
+v_aasta int);
+select * from film
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Words, The', 155, 'Yetta Silcock', 1997);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Last of the Dogmen', 192, 'Sammy Fenton', 1995);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Wondrous Oblivion', 117, 'Jeanine Radborne', 2008);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Living Ghost, The', 159, 'Abran Symms', 2006);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Clerks', 142, 'Carolyn Brahms', 1986);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('God Bless America', 133, 'Onida Mosedill', 2009);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Return of Swamp Thing, The', 231, 'Sallee Francey', 2002);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Peculiarities of the National Fishing (Osobennosti natsionalnoy rybalki)', 221, 'Laurene Dutchburn', 2011);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Shottas', 216, 'Nancee Guisot', 2005);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Night Across the Street (La noche de enfrente)', 290, 'Dwain Skally', 1999);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('It''s a Bird', 182, 'Giulia Sill', 1994);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Zindagi Na Milegi Dobara', 271, 'Cecelia Emby', 2001);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Idolmaker, The', 193, 'Leon Tombleson', 1985);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Bikini Summer II', 117, 'Bobbe Bowater', 2004);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Anniversary Party, The', 134, 'Jenine Zannini', 1986);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Spirit of ''76, The', 268, 'Ripley O''Loughnan', 2003);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('With Six You Get Eggroll', 202, 'Storm Issitt', 1998);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Lady Eve, The', 229, 'Dukey MacGlory', 2011);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Atlas Shrugged: Part 1', 152, 'Shaw Slayford', 2005);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Mission to Moscow', 240, 'Diane Leake', 1998);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Sorcerer''s Apprentice, The', 213, 'Charo Mc Gaughey', 2002);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Lords of Dogtown', 275, 'Rafaelia Napper', 2009);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Yobi, The Five-Tailed Fox', 138, 'Granthem Demko', 2008);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('A Story of Children and Film', 273, 'Kim Huckle', 1990);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Amongst Friends', 245, 'Davina Cazereau', 2011);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Guardians of the Galaxy', 206, 'Lancelot Castell', 1999);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Trip, The', 283, 'Hedi Harriskine', 2012);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Stars and Stripes Forever', 150, 'Dulcine Sweett', 2008);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Two Faces of January, The', 209, 'Maddie Melland', 1993);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Lady is Willing, The', 148, 'Jami MacAndrew', 2011);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Sublime', 150, 'Paloma Olle', 2006);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Tight Spot', 113, 'Claresta Ferier', 1993);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Being Elmo: A Puppeteer''s Journey', 256, 'Berny Samudio', 2001);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('East Side, West Side', 245, 'Edwin Skelhorn', 1995);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Trail of the Screaming Forehead', 242, 'Rhianon Cassel', 1995);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Ruby Cairo', 264, 'Edwin Carl', 2005);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Freeway', 227, 'Saundra Barfitt', 1979);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Abbott and Costello Go to Mars', 229, 'Beth Wigg', 1997);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('It Came from Hollywood', 156, 'Guillermo Mesnard', 1994);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Rockaway', 134, 'Giffard Wallworth', 2012);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Kautokeino Rebellion, The (Kautokeino-opprøret)', 228, 'Dylan Verlinde', 1989);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('September', 198, 'Reyna Ebbs', 1989);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Nun, The (La monja) ', 164, 'Millie Masser', 1997);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Beautiful Lies (De vrais mensonges) (Full Treatment)', 288, 'Sallie Bailey', 2004);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Pauline & Paulette (Pauline en Paulette)', 120, 'Wat Cail', 1993);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Patty Hearst', 250, 'Sebastian Basnett', 2009);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('This Thing With Sarah', 165, 'Felicity Jaye', 1998);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Story of G.I. Joe', 275, 'Brannon Stonehewer', 1984);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('In a World...', 242, 'Patrica Maffetti', 2007);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Hatchet', 267, 'Michaella Primo', 1991);
 
