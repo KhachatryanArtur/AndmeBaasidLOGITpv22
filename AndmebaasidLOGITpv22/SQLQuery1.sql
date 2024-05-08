c694b2bcfb53ddaab00db973c22bc703252717e4
Artur Khachatryan <artur.hatsatrjan@gmail.com>
2024-05-08 12:48:49 +0300

    Departmenti täitmine

--- AndmebaasidLOGITpv22/SQLQuery1.sql
+++ AndmebaasidLOGITpv22/SQLQuery1.sql
@@ -179,7 +179,7 @@ Salary nvarchar(50),
 DepartmentId int
 )
 
---?
+--täidame tabeli Department
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
 values (1, 'IT', 'London', 'Rick')
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
